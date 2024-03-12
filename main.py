e180b5bbff3161821d9edd7d2f2892676b521fd8
SwitcheRka <ooomaxooo2005@gmail.com>
2024-03-12 09:15:58 +0300

    Added output to txt.file
    Added output to txt.file
    +imported OS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as BS
+import os
 
 link = "https://dnd.su/race/"
 responce = requests.get(link).text
@@ -15,4 +16,12 @@ for i in range(length):
     anime = block.find_all('span', class_ = "article_title")[i].text
     result += anime + ", "
 
-print(result)
+def create_file(result):
+    my_file = open("DnDsu_races.txt", "w+")
+    my_file.write(result)
+    fileContents = my_file.read()
+    print(fileContents)
+    my_file.close()
+
+
+create_file(result)
