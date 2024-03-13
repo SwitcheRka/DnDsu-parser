16f1a41396a9ec4f8cfd768d72b1839ef1a42258
SwitcheRka <ooomaxooo2005@gmail.com>
2024-03-13 17:38:07 +0300

    added saving information to Word file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BS
 import os
+import docx
 
 link = "https://dnd.su/race/"
 responce = requests.get(link).text
@@ -16,12 +17,17 @@ for i in range(length):
     anime = block.find_all('span', class_ = "article_title")[i].text
     result += anime + ", "
 
+#def create_file(result):
+#    my_file = open("DnDsu_races.txt", "w+")
+#    my_file.write(result)
+#    fileContents = my_file.read()
+#    print(fileContents)
+#    my_file.close()
+
 def create_file(result):
-    my_file = open("DnDsu_races.txt", "w+")
-    my_file.write(result)
-    fileContents = my_file.read()
-    print(fileContents)
-    my_file.close()
+    doc = docx.Document()
+    doc.add_paragraph(result)
+    doc.save('Test.docx')
 
-
+print(result)
 create_file(result)
